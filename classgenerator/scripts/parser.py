5bcbd05cc79491006c9812e7434f526e10c3329d
Michael Saunby <mike@saunby.net>
2020-10-27 21:34:57 +0000

    .

--- classgenerator/scripts/parser.py
+++ classgenerator/scripts/parser.py
@@ -1,24 +1,28 @@
 from sys import argv
 import json
+from python_template import PythonTemplate
 
 class Parser:
     def __init__(self, args=None):
         self._args = args if args is not None else []
 
     def check_args(self):
-        print(len(self._args))
         self._injson = self._args[1]
 
     def parse(self):
         infile = open(self._injson,'r')
         data = json.load(infile)
+        t = PythonTemplate()
         for cl in data:
-            print(cl['class'])
+            #print(cl['class'])  
+            classname = cl['class']
+            print(t.class_defn(classname))
+            print()
 
 
 if __name__ == '__main__':
     p = Parser(argv)
-    breakpoint()
+    #breakpoint()
     p.check_args()
     p.parse()
     
